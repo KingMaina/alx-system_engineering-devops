e7f125b380264a4bf18513a59a0aa4e1307d07ac
Andrew <drew29799@gmail.com>
2023-11-29 23:58:54 +0300

    Fix: index files and puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -2,17 +2,10 @@ package { 'nginx':
   ensure => installed,
 }
 
-# Web server directory
-file { 'Website directory':
-  ensure  => directory,
-  path    => '/var/www/html',
-  require => Package['nginx'],
-}
-
 # Ensure index file is present and contains 'Hello World!'
 file { 'Website index file':
   ensure  => present,
-  path    => '/var/www/html/index.html',
+  path    => '/var/www/html/index.nginx-debian.html',
   content => 'Hello World!',
   require => Package['nginx'],
 }
@@ -22,21 +15,10 @@ file { 'Website 404 file':
   ensure  => present,
   path    => '/var/www/html/404.html',
   content => "Ceci n'est pas une page",
-  require => Package['nginx'],
-}
-
-# Ensure nginx redirects to a custom page when accessing /redirect_me
-file_line { 'nginx redirect':
-  ensure  => present,
-  path    => '/etc/nginx/sites-available/default',
-  line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-  require => Package['nginx'],
-  notify  => Service['nginx'],
 }
 
 # Ensure nginx is running
 service { 'nginx':
   ensure  => running,
-  enable  => true,
   require => Package['nginx']
 }
