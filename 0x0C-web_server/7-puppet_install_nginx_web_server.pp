b0a14b1d5534831f56bb30a3845efa0a4813c705
Andrew <drew29799@gmail.com>
2023-11-29 23:18:51 +0300

    Add: Puppet config
    * Added puppet resources to manage a server using nginx.
    * Fixed default index file name for nginx to serve

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,19 +1,40 @@
-# Puppet manifest to install nginx
 package { 'nginx':
   ensure => installed,
 }
 
-# Check 404 file
-file { '/var/www/html/404.html':
-  content => "Ceci n'est pas une page\n",
+# Web server directory
+file { 'Website directory':
+  ensure  => directory,
+  path    => '/var/www/html',
+  require => Package['nginx'],
 }
 
-# Check index file
-file { '/var/www/html/index.html':
+# Ensure index file is present and contains 'Hello World!'
+file { 'Website index file':
+  ensure  => present,
+  path    => '/var/www/html/index.nginx-debian.html',
   content => 'Hello World!',
 }
 
+# Ensure 404 file is present and contains 'Ceci n'est pas une page'
+file { 'Website 404 file':
+  ensure  => present,
+  path    => '/var/www/html/404.html',
+  content => "Ceci n'est pas une page",
+}
+
+# Ensure nginx redirects to a custom page when accessing /redirect_me
+file_line { 'nginx redirect':
+  ensure  => present,
+  path    => '/etc/nginx/sites-available/default',
+  line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
+
+# Ensure nginx is running
 service { 'nginx':
   ensure  => running,
-  require => Package['nginx'],
+  enable  => true,
+  require => Package['nginx']
 }
